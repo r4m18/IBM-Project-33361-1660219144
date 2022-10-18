9ca0a4d305060fe682e36fc0fcb34b8d96a686d7
Vinsmokeshiva <108256136+Vinsmokeshiva@users.noreply.github.com>
2022-10-18 20:38:37 +0530

    Update app.py

--- Assignments/SHIVAGANESH PANDIAN S/Assignment 2/app.py
+++ Assignments/SHIVAGANESH PANDIAN S/Assignment 2/app.py
@@ -1,1 +1,80 @@
+from site import USER_BASE
+from flask import Flask, render_template, url_for, request, redirect
+import sqlite3 assql
 
+ 
+app=Flask(__name__)
+app.secret_key ='abcdxyz'
+
+@app.route('/')
+defhome():
+    con =sql.connect('user_base.db')
+    con.row_factory=sql.Row
+    cur=con.cursor()
+    cur.execute('select *from user')
+
+    users= cur.fetchall()
+    con.close()
+    returnrender_template('index.html',users=users)
+
+@app.route('/about')
+defabout():
+    returnrender_template('about.html')
+@app.route('/signin')
+defsignin():
+    returnrender_template('signin.html')
+@app.route('/signup')
+defsingup():
+    returnrender_template('signup.html')
+@app.route('/user/<id>')
+defuser_page(id):
+    withsql.connect('user_base.db') as con:
+        con.row_factory=sql.Row
+        cur =con.cursor()
+        cur.execute(f'SELECT * FROM user WHERE email="{id}"')
+        user = cur.fetchall()
+    returnrender_template("user_info.html", user=user[0])
+
+@app.route('/accessbackend', methods=['POST','GET'])
+defaccessbackend():
+    ifrequest.method == "POST":
+        try:
+            firstname=request.form['firstname']
+            lastname=request.form['lastname']
+            e_mail= request.form['email']
+            phone=request.form['phone']
+            password=request.form['password']
+            dob=request.form['dob']
+
+            withsql.connect('user_base.db') as con:
+                cur =con.cursor()
+                cur.execute('INSERT INTO user (firstname, lastname, email, phone, password, dob ) VALUES(?,?,?,?,?,?)', (str(firstname),str(lastname),str(e_mail),str(phone),str(password),str(dob)))
+                con.commit()
+                msg='u r resgistered!'
+    
+        except:
+            con.rollback()
+            msg='some error'      
+
+        finally:
+            print(msg)
+            return redirect(url_for('home'))
+    else:
+        try:
+            tue=request.args.get('email')
+            tup=request.args.get('password')
+            print(tue,tup)
+            withsql.connect('user_base.db') as con:
+                con.row_factory=sql.Row
+                cur=con.cursor()
+                cur.execute(f'SELECT password FROM user WHERE email="{tue}"')
+                user= cur.fetchall()
+        except:
+            print('error')
+            con.rollback()
+        finally:
+            iflen(user) >0:
+                iftup == user[0][0]:
+                    return redirect(url_for("user_page",id=tue))
+                print(user[0][0])
+            return redirect(url_for('signin'))
